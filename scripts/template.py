b06c8fddd23bf63c5710138baf48013c2d9aebc7
ks6088ts <ks6088ts@gmail.com>
2025-08-23 15:42:41 +0900

    update script template

--- scripts/template.py
+++ scripts/template.py
@@ -7,14 +7,20 @@ from dotenv import load_dotenv
 from template_python.loggers import get_logger
 from template_python.settings import Settings
 
-# Initialize the Typer application
 app = typer.Typer(
     add_completion=False,
     help="template-python CLI",
 )
 
-# Set up logging
 logger = get_logger(__name__)
+
+
+def set_verbose_logging(
+    verbose: bool,
+):
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
 
 
 @app.command()
@@ -32,19 +38,15 @@ def hello(
         typer.Option("--verbose", "-v", help="Enable verbose output"),
     ] = False,
 ):
-    # Set up logging
-    if verbose:
-        logger.setLevel(logging.DEBUG)
+    set_verbose_logging(verbose)
 
     logger.debug(f"This is a debug message with name: {name}")
-    print(f"Hello {name}")
-    settings = Settings()
-    print(f"Settings from .env: {settings.model_dump_json(indent=2)}")
+    logger.info(f"Settings from .env: {Settings().model_dump_json(indent=2)}")
 
 
 if __name__ == "__main__":
-    load_dotenv(
+    assert load_dotenv(
         override=True,
         verbose=True,
-    )
+    ), "Failed to load environment variables"
     app()
